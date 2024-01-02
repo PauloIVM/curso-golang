dd64fce93a87c45ac20bd50672c4521db1fecba9
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-01-02 13:21:46 -0300

    chore: add comments

--- 18 - Concorrência/4 - Canais com Buffer/canais-com-buffer.go
+++ 18 - Concorrência/4 - Canais com Buffer/canais-com-buffer.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	canal := make(chan string, 200)
+	// INFO: O canal com buffer só bloqueia se atingir a capacidade máxima de escrita ou
+	// mínima de leitura.
+	canal := make(chan string, 4)
 	canal <- "Olá Mundo!"
 	canal <- "Programando em Go!"
+	canal <- "Programando em Go De Novo!"
 	canal <- "Programando em Go De Novo!"
 
 	mensagem := <-canal
 	mensagem2 := <-canal
+	mensagem3 := <-canal
+	mensagem4 := <-canal
 
 	fmt.Println(mensagem)
 	fmt.Println(mensagem2)
+	fmt.Println(mensagem3)
+	fmt.Println(mensagem4)
 }
--- 18 - Concorrência/6 - Padrões de Concorrência/6.1 - Worker Pools/worker-pools.go
+++ 18 - Concorrência/6 - Padrões de Concorrência/6.1 - Worker Pools/worker-pools.go
@@ -1,26 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+// INFO: Esse patern pode atribuir ao "resultados" elementos com o index "desalinhados"
+// com os elementos do "tarefas"? Sim... os workers vão executar bem fora de ordem. Então
+// numa implementação desse tipo, é provável que eu precise jogar no "resultados" uma structure
+// com algum atributo que referencie qual "tarefa" gerou o respectivo resultado.
 func main() {
 	tarefas := make(chan int, 45)
 	resultados := make(chan int, 45)
 
-	go worker(tarefas, resultados)
-	go worker(tarefas, resultados)
-	go worker(tarefas, resultados)
-	go worker(tarefas, resultados)
+	// INFO: Se eu manter um único worker, então será printado os resultados em ordem. Mas se eu
+	// adicionar mais workers, então será printado os resultados fora de ordem. Se eu tirar o sleep,
+	// mesmo com mais de um worker, então ele volta a printar ordenadamente... não é tão óbvio o
+	// problema
 
-	for i := 0; i < 45; i++ {
+	// TODO: Um desafio interessante: Modificar a structure de forma que eu possa reordená-las
+	// (nesse caso seria simplesmente ordenar já que é um array de inteiros), e então usar o pattern de
+	// concorrêcia e por fim ordenar os resultados de acordo com a correspondêcia do array de tarefas.
+
+	go worker2(tarefas, resultados)
+	go worker2(tarefas, resultados)
+	go worker2(tarefas, resultados)
+	go worker2(tarefas, resultados)
+
+	for i := 1; i < 45; i++ {
 		tarefas <- i
 	}
 	close(tarefas)
 
-	for i := 0; i < 45; i++ {
+	for i := 1; i < 45; i++ {
 		resultado := <-resultados
 		fmt.Println(resultado)
 	}
 
+}
+
+func worker2(tarefas <-chan int, resultados chan<- int) {
+	for numero := range tarefas {
+		resultados <- randomFunc(numero)
+	}
+}
+
+func randomFunc(posicao int) int {
+	time.Sleep(time.Second / time.Duration(posicao))
+	return posicao
 }
 
 func worker(tarefas <-chan int, resultados chan<- int) {
@@ -33,6 +60,5 @@ func fibonacci(posicao int) int {
 	if posicao <= 1 {
 		return posicao
 	}
-
 	return fibonacci(posicao-2) + fibonacci(posicao-1)
 }
